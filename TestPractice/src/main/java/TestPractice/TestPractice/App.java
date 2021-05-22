a9232cab48a3d2824246badfb8a9f0478e9df5a3
ramkrishzkvr <ramkrishzkvr@gmail.com>
2021-05-23 04:51:36 +0530

    first commit

---
+++ TestPractice/src/main/java/TestPractice/TestPractice/App.java
@@ -0,0 +1,13 @@
+package TestPractice.TestPractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
